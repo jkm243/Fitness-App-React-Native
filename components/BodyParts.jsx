41f1424bacab06284744698c8e9779e3aaf79804
Jacques Katsuva <40735390+jkm243@users.noreply.github.com>
2024-02-27 00:20:02 +0300

    adding searchbar

--- components/BodyParts.jsx
+++ components/BodyParts.jsx
@@ -1,6 +1,7 @@
-import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native'
-import React from 'react'
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { View, Text, FlatList, TouchableOpacity, Image} from 'react-native'
+import React, { useState, useMemo } from 'react'
+import SearchBar from 'react-native-platform-searchbar';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { bodyParts } from '../constants';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useRouter } from 'expo-router';
@@ -8,54 +9,64 @@ import Animated, { FadeInDown } from 'react-native-reanimated';
 
 export default function BodyParts() {
     const router = useRouter();
-  return (
-    <View className="mx-4">
-      <Text style={{fontSize: hp(3)}} className="font-semibold text-neutral-700">
-        Exercises
-      </Text>
+    const [searchInput, setSearchInput] = useState('');
 
-      <FlatList
-        data={bodyParts}
-        numColumns={2}
-        keyExtractor={item=> item.name}
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{paddingBottom: 50, paddingTop: 20}}
-        columnWrapperStyle={{
-            justifyContent: 'space-between'
-        }}
-        renderItem={({item, index})=> <BodyPartCard router={router} index={index} item={item} />}
-      />
-    </View>
-  )
+    const filteredBodyParts = useMemo(() => {
+        if (!searchInput) return bodyParts;
+        return bodyParts.filter(item => item.name.toLowerCase().includes(searchInput.toLowerCase()));
+    }, [searchInput]);
+
+    return (
+        <View className="mx-4">
+            <Text style={{ fontSize: hp(3) }} className="font-semibold text-neutral-700">
+                Exercises
+            </Text>
+
+            <SearchBar placeholder='Search for exercises...' platform="default" cancelText="Cancel" barTintColor="#fff"
+                onChangeText={(text) => setSearchInput(text)} />
+
+            <FlatList
+                data={filteredBodyParts}
+                numColumns={2}
+                keyExtractor={item => item.name}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ paddingBottom: 50, paddingTop: 20 }}
+                columnWrapperStyle={{
+                    justifyContent: 'space-between'
+                }}
+                renderItem={({ item, index }) => <BodyPartCard router={router} index={index} item={item} />}
+            />
+        </View>
+    )
 }
 
-const BodyPartCard = ({item, router, index})=>{
+const BodyPartCard = ({ item, router, index }) => {
     return (
-        <Animated.View entering={FadeInDown.duration(400).delay(index*200).springify()}>
+        <Animated.View entering={FadeInDown.duration(400).delay(index * 200).springify()}>
             <TouchableOpacity
-                onPress={()=> router.push({pathname: '/exercises', params: item})}
-                style={{width: wp(44), height: wp(52)}}
+                onPress={() => router.push({ pathname: '/exercises', params: item })}
+                style={{ width: wp(44), height: wp(52) }}
                 className="flex justify-end p-4 mb-4">
-                    <Image 
-                        source={item.image}
-                        resizeMode='cover'
-                        style={{width: wp(44), height: wp(52)}}
-                        className="rounded-[35px] absolute"
-                    />
-                    <LinearGradient
-                        colors={['transparent', 'rgba(0,0,0,0.9)']}
-                        style={{width: wp(44), height: hp(15)}}
-                        start={{x: 0.5, y: 0}}
-                        end={{x: 0.5, y: 1}}
-                        className="absolute bottom-0 rounded-b-[35px]"
-                    />
+                <Image
+                    source={item.image}
+                    resizeMode='cover'
+                    style={{ width: wp(44), height: wp(52) }}
+                    className="rounded-[35px] absolute"
+                />
+                <LinearGradient
+                    colors={['transparent', 'rgba(0,0,0,0.9)']}
+                    style={{ width: wp(44), height: hp(15) }}
+                    start={{ x: 0.5, y: 0 }}
+                    end={{ x: 0.5, y: 1 }}
+                    className="absolute bottom-0 rounded-b-[35px]"
+                />
 
-                    <Text
-                        style={{fontSize: hp(2.3)}}
-                        className="text-white font-semibold text-center tracking-wide"
-                    >
-                        {item?.name}
-                    </Text>
+                <Text
+                    style={{ fontSize: hp(2.3) }}
+                    className="text-white font-semibold text-center tracking-wide"
+                >
+                    {item?.name}
+                </Text>
             </TouchableOpacity>
         </Animated.View>
     )
